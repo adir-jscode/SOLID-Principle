115e05a113ea84f335c2ef601d40eb370465ea8e
Adir <adir.jscode@gmail.com>
2024-06-16 17:19:21 +0600

    Updated with some info

--- Dependency Inverstion/Program.cs
+++ Dependency Inverstion/Program.cs
@@ -2,6 +2,13 @@
 {
     class Program
     {
+
+        /*
+        1. High-level modules should not depend on low-level modules. Both should depend on abstractions.
+        2. Abstractions should not depend on details. Details should depend on abstractions.
+        3. Dependency Inversion Principle is a set of guidelines to follow to implement Dependency Inversion Principle.
+        4. Dependency Injection is a design pattern that implements the Dependency Inversion Principle.
+         */
         static void Main(string[] args)
         {
             DataAccessLayer dataAccessLayer = new DataAccessLayer(new Service.FileLogger());
